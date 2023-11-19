aac338fa9fb124f92aac3bcff878a9ed16a88c17
Bryan de Bourbon <bryandebourbon@gmail.com>
2023-11-18 19:25:21 -0500

    Initial Commit

---
+++ SpendTracker/SpendTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendTrackerApp.swift
+//  SpendTracker
+//
+//  Created by Bryan de Bourbon on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SpendTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
